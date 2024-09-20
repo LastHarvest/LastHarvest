865308ad39ccfbda252cd662de6eeef923d46c7e
RUIZ Fannie <fannie.ruiz@etu.umontpellier.fr>
2024-09-20 17:44:53 +0200

    1er test class

---
+++ Person.py
@@ -0,0 +1,8 @@
+class player:
+    def __init__(self,id,name,position,possession,points):
+        self.id = id
+        self.name = name
+        self.position = position
+        self.possession = possession
+        self.points = points
+
