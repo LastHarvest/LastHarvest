47a08079c21e30ed4bc2c77389e95450a4518842
alderaan <anaismackow@gmail.com>
2024-09-21 01:17:04 +0200

    [+] Game, Player, Resource Class [+] Launcher (main) with a windows (6x6 grid) Implementation of 2 players and random resources that disappear from the grid if a player pass on the cell

---
+++ Resource.py
@@ -0,0 +1,7 @@
+class Ressource:
+    def __init__(self,id,value,position,type,points):
+        self.id = id
+        self.value = value
+        self.position = position
+        self.type = type
+        self.points = points
