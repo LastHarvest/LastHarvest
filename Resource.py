a833fd8ed2b5e91833398c7671bc726331203c2d
alderaan <anaismackow@gmail.com>
2024-09-21 14:31:34 +0200

    main?

--- Resource.py
+++ Resource.py
@@ -1,7 +1,37 @@
-class Ressource:
-    def __init__(self,id,value,position,type,points):
-        self.id = id
-        self.value = value
-        self.position = position
-        self.type = type
-        self.points = points
+class Resource:
+    def __init__(self, id: int, value: int, position: tuple, type: str, isFree: bool):
+        self.__id = id
+        self.__value = value
+        self.__position = position
+        self.__type = type
+        self.__isFree = isFree
+
+    def get_id(self) -> int:
+        return self.__id
+
+    def get_value(self) -> int:
+        return self.__value
+
+    def get_position(self) -> tuple:
+        return self.__position
+
+    def get_type(self) -> str:
+        return self.__type
+
+    def get_isFree(self) -> bool:
+        return self.__isFree
+
+    def set_id(self, id: int):
+        self.__id = id
+
+    def set_value(self, value: int):
+        self.__value = value
+
+    def set_position(self, position: tuple):
+        self.__position = position
+
+    def set_type(self, type: str):
+        self.__type = type
+
+    def set_isFree(self ):
+        self.__isFree = False
